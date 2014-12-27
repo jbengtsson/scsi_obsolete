6d07d3924f11edd11a7bff870e87f62c30b96499
Johan Bengtsson <jbngtssn@gmail.com>
2014-12-27 13:10:11 -0500

    Lex/Yacc based lattice parser.

--- scsi_src/src/t2elem.cc
+++ scsi_src/src/t2elem.cc
@@ -21,6 +21,76 @@ int   FieldMap_filetype = 2;
 
 
 template<typename T>
+void spline_(const double x[], const T y[], const int n, const double yp1,
+	     const double ypn, T y2[])
+{
+  int     i,k;
+  double  sig;
+  T       p, u[n], qn, un;
+
+  if (yp1 > 0.99e30)
+    y2[1] = u[1] = 0.0;
+  else {
+    y2[1] = -0.5;
+    u[1] = (3.0/(x[2]-x[1]))*((y[2]-y[1])/(x[2]-x[1])-yp1);
+  }
+  for (i = 2; i <= n-1; i++) {
+    sig = (x[i]-x[i-1])/(x[i+1]-x[i-1]);
+    p = sig*y2[i-1]+2.0;
+    y2[i] = (sig-1.0)/p;
+    u[i] = (y[i+1]-y[i])/(x[i+1]-x[i]) - (y[i]-y[i-1])/(x[i]-x[i-1]);
+    u[i] = (6.0*u[i]/(x[i+1]-x[i-1])-sig*u[i-1])/p;
+  }
+  if (ypn > 0.99e30)
+    qn = un = 0.0;
+  else {
+    qn = 0.5;
+    un = (3.0/(x[n]-x[n-1]))*(ypn-(y[n]-y[n-1])/(x[n]-x[n-1]));
+  }
+  y2[n] = (un-qn*u[n-1])/(qn*y2[n-1]+1.0);
+  for (k = n-1; k >= 1; k--)
+    y2[k] = y2[k]*y2[k+1]+u[k];
+}
+
+
+void splie2_(const double x1a[], const double x2a[], double **ya,
+	     const int m, const int n, double **y2a)
+{
+  int  j;
+
+  for (j = 1; j <= m; j++)
+    spline_(x2a,ya[j],n,1.0e30,1.0e30,y2a[j]);
+}
+
+
+template<typename T, typename U>
+void splint_(const double xa[], const U ya[], const U y2a[],
+	     const int n, const T &x, T &y)
+{
+  int     klo,khi,k;
+  double  h;
+  T       a,b;
+
+  klo = 1;
+  khi = n;
+  while (khi-klo > 1) {
+    k = (khi+klo) >> 1;
+    if (xa[k] > x) khi = k;
+    else klo = k;
+  }
+  h = xa[khi]-xa[klo];
+  if (h == 0.0) {
+    printf("Bad xa input to routine splint_\n");
+    exit(1);
+  }
+
+  a = (xa[khi]-x)/h;
+  b = (x-xa[klo])/h;
+  y = a*ya[klo]+b*ya[khi]+((a*a*a-a)*y2a[klo]+(b*b*b-b)*y2a[khi])*(h*h)/6.0;
+}
+
+
+template<typename T>
 void splin2_(const double x1a[], const double x2a[], double **ya, double **y2a,
 	     const int m, const int n, const T &x1, const T &x2, T &y)
 {
@@ -3157,76 +3227,6 @@ void Wiggler_Init(int Fnum1)
   }
 }
 #undef order
-
-
-template<typename T>
-void spline_(const double x[], const T y[], const int n, const double yp1,
-	     const double ypn, T y2[])
-{
-  int     i,k;
-  double  sig;
-  T       p, u[n], qn, un;
-
-  if (yp1 > 0.99e30)
-    y2[1] = u[1] = 0.0;
-  else {
-    y2[1] = -0.5;
-    u[1] = (3.0/(x[2]-x[1]))*((y[2]-y[1])/(x[2]-x[1])-yp1);
-  }
-  for (i = 2; i <= n-1; i++) {
-    sig = (x[i]-x[i-1])/(x[i+1]-x[i-1]);
-    p = sig*y2[i-1]+2.0;
-    y2[i] = (sig-1.0)/p;
-    u[i] = (y[i+1]-y[i])/(x[i+1]-x[i]) - (y[i]-y[i-1])/(x[i]-x[i-1]);
-    u[i] = (6.0*u[i]/(x[i+1]-x[i-1])-sig*u[i-1])/p;
-  }
-  if (ypn > 0.99e30)
-    qn = un = 0.0;
-  else {
-    qn = 0.5;
-    un = (3.0/(x[n]-x[n-1]))*(ypn-(y[n]-y[n-1])/(x[n]-x[n-1]));
-  }
-  y2[n] = (un-qn*u[n-1])/(qn*y2[n-1]+1.0);
-  for (k = n-1; k >= 1; k--)
-    y2[k] = y2[k]*y2[k+1]+u[k];
-}
-
-
-void splie2_(const double x1a[], const double x2a[], double **ya,
-	     const int m, const int n, double **y2a)
-{
-  int  j;
-
-  for (j = 1; j <= m; j++)
-    spline_(x2a,ya[j],n,1.0e30,1.0e30,y2a[j]);
-}
-
-
-template<typename T, typename U>
-void splint_(const double xa[], const U ya[], const U y2a[],
-	     const int n, const T &x, T &y)
-{
-  int     klo,khi,k;
-  double  h;
-  T       a,b;
-
-  klo = 1;
-  khi = n;
-  while (khi-klo > 1) {
-    k = (khi+klo) >> 1;
-    if (xa[k] > x) khi = k;
-    else klo = k;
-  }
-  h = xa[khi]-xa[klo];
-  if (h == 0.0) {
-    printf("Bad xa input to routine splint_\n");
-    exit(1);
-  }
-
-  a = (xa[khi]-x)/h;
-  b = (x-xa[klo])/h;
-  y = a*ya[klo]+b*ya[khi]+((a*a*a-a)*y2a[klo]+(b*b*b-b)*y2a[khi])*(h*h)/6.0;
-}
 
 
 // instantiate
