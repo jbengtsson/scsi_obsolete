5c25a509f7f564fda2cbeabb8a2055568cc73f61
Johan Bengtsson <jbngtssn@gmail.com>
2014-12-29 07:59:53 -0500

    Work on Python API.

--- scsi_src/python/shared_lib.sh
+++ scsi_src/python/shared_lib.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-g++ -shared -fPIC -I../inc -I/usr -I/usr/include/python2.6 -L../lib -lscsi -lstdc++ -lgsl -lgslcblas pyscsi_wrap.cxx -o _pyscsi.so
+swig -c++ -python -globals gv pyscsi.i
+
+gcc -shared -fPIC -I../inc -I/usr -I/usr/include/python2.6 \
+    -L../lib -lscsi -lgsl -lstdc++ -lgslcblas pyscsi_wrap.cxx \
+    -o _pyscsi.so
+
+cp _pyscsi.so ../lib/.
--- scsi_src/src/shared_lib.sh
+++ scsi_src/src/shared_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gcc -DHAVE_CONFIG_H -I. -I../.. -I../inc -I/usr -g -O0 -Wall -Wno-error=all -fno-implicit-templates -fPIC -MT scsi_lib.o -MD -MP -MF .deps/scsi_lib.Tpo -c -o libscsi.so scsi_lib.cc -shared
+gcc -shared -fPIC -I../inc -I/usr scsi_lib.cc -o libscsi.so
 
 cp libscsi.so ../lib/.
