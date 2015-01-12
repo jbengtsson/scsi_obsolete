0212f1d6f9e64fbf87b06659824bee7b311d900e
Johan Bengtsson <jbngtssn@gmail.com>
2015-01-12 16:56:26 -0500

    Code clean-up for Python API.

--- scsi_src/ctypes/pyscsi.py
+++ scsi_src/ctypes/pyscsi.py
@@ -8,7 +8,6 @@ home_dir = os.path.expanduser('~')
 
 #libc = cdll.LoadLibrary('libc.so.6')
 #libc++ =  cdll.LoadLibrary('libstdc++.so.6')
-
 #gslcblas = CDLL('libgslcblas.so', mode=RTLD_GLOBAL)
 #gsl = CDLL('libgsl.so', mode=RTLD_GLOBAL)
 
@@ -18,49 +17,79 @@ scsi = cdll.LoadLibrary(home_dir+'/git_repos/scsi/scsi_src/lib/libscsi.so')
 #import pyscsi
 
 Vector2 = c_double*2
-vector  = c_double*6
+Vector  = c_double*6
+Matrix  = Vector*6
 
 partsName = c_char*150
 PartsKind = c_long
 
 class elemtype(Structure):
     _fields_ = (('PName', partsName),
-                ('PL', c_double),
+                ('PL',    c_double),
                 ('PKind', PartsKind),
-                ('UU', c_void_p))
+                ('UU',    c_void_p))
 
 class CellType(Structure):
-    _fields_ = (('Fnum', c_long),
-                ('Knum', c_long),
-                ('S', c_double),
-                ('dS', Vector2),
-                ('dT', Vector2),
-                ('Elem', elemtype),
-                ('Nu', Vector2),
-                ('Alpha', Vector2),
-                ('Beta', Vector2),
-                ('Eta', Vector2),
-                ('Etap', Vector2),
-                ('BeamPos', vector))
+    _fields_ = (('Fnum',    c_long),
+                ('Knum',    c_long),
+                ('S',       c_double),
+                ('dS',      Vector2),
+                ('dT',      Vector2),
+                ('Elem',    elemtype),
+                ('Nu',      Vector2),
+                ('Alpha',   Vector2),
+                ('Beta',    Vector2),
+                ('Eta',     Vector2),
+                ('Etap',    Vector2),
+                ('BeamPos', Vector))
 
 class globvalrec(Structure):
-    nLocMax1 = 10001
-    _fields_ = (('dPcommon', c_double),
+    _fields_ = [('dPcommon',   c_double),
                 ('dPparticle', c_double),
-                ('maxamplH', Vector2 * nLocMax1),
-                ('maxamplV', Vector2 * nLocMax1),
-                ('RFAcceptance', c_double),
-                ('TotalTune', Vector2),
-                ('Omega', c_double),
-                ('U0', c_double),
-                ('Alphac', c_double),
-                ('Chrom', Vector2),
-                ('Energy', c_double),
-                ('Cell_nLoc', c_long))
+                ('delta_RF',   c_double),
+                ('TotalTune',  Vector2),
+                ('Omega',      c_double),
+                ('U0',         c_double),
+                ('Alphac',     c_double),
+                ('Chrom',      Vector2),
+                ('Energy',     c_double),
+                ('Cell_nLoc',  c_long),
+                ('Elem_nFam',  c_long),
+                ('CODimax',    c_long),
+
+                ('CODeps',     c_double),
+                ('CODvect',    Vector),
+                ('bpm',        c_int),
+                ('hcorr',      c_int),
+                ('vcorr',      c_int),
+                ('qt',         c_int),
+                ('gs',         c_int),
+                ('ge',         c_int),
+                ('OneTurnMat', Matrix),
+                ('Ascr',       Matrix),
+                ('Ascrinv',    Matrix),
+                ('Vr',         Matrix),
+                ('Vi',         Matrix)
+                ]
 
 print scsi
+print c_bool.in_dll(scsi, 'trace')
+print c_int.in_dll(scsi, 'no_tps')
+print c_int.in_dll(scsi, 'nv_tps')
+print scsi.globval
+print dir(scsi.globval)
+
 globval = cast(scsi.globval, POINTER(globvalrec))[0]
+print dir(globval)
 print globval.dPcommon
+for ps in globval.CODvect:
+    sys.stdout.write('%6.3f' % ps)
+sys.stdout.write('\n')
+
+print c_double.in_dll(scsi, 'globval')
+globval = POINTER(globvalrec).in_dll(scsi, 'globval')
+print dir(globval)
+print globval
 
 #scsi.Read_Lattice(home_dir+'/git_repos/scsi/scsi_src/glps/tracy_1')
    
