2df42e8566322e2837200038b801a7c8ad90f140
Johan Bengtsson <jbngtssn@gmail.com>
2015-01-26 20:39:48 -0500

    Added README.txt.

--- scsi_src/src/usr_lib.cc
+++ scsi_src/src/usr_lib.cc
@@ -3,21 +3,21 @@
 void get_bn(const string type, const int Fnum, const int Knum,
 	    const int n, double &bn, double &an)
 {
-  ElemType  elem;
+  MpoleType *M;
 
   if ((n < 1) || (n > HOMmax)) {
     cout << "get_bn: n < 1 (" << n << ")" << endl;
     exit(1);
   }
 
-  elem = Cell[Elem_GetPos(Fnum, Knum)].Elem;
+  M = static_cast<MpoleType*>(Cell[Elem_GetPos(Fnum, Knum)].Elem);
 
   if (type.compare("dsgn") == 0) {
-    bn = elem.M->bnpar[HOMmax+n]; an = elem.M->bnpar[HOMmax-n];
+    bn = M->bnpar[HOMmax+n]; an = M->bnpar[HOMmax-n];
   } else if (type.compare("sys") == 0) {
-    bn = elem.M->bnsys[HOMmax+n]; an = elem.M->bnsys[HOMmax-n];
+    bn = M->bnsys[HOMmax+n]; an = M->bnsys[HOMmax-n];
   } else if (type.compare("rms") == 0) {
-    bn = elem.M->bnrms[HOMmax+n]; an = elem.M->bnrms[HOMmax-n];
+    bn = M->bnrms[HOMmax+n]; an = M->bnrms[HOMmax-n];
   } else {
     cout << "get_bn: undef. type" << type << endl;
     exit(1);
@@ -28,21 +28,21 @@ void get_bn(const string type, const int Fnum, const int Knum,
 void set_bn(const string type, const int Fnum, const int Knum,
 	    const int n, const double bn, const double an)
 {
-  ElemType  elem;
+  MpoleType *M;
 
   if ((n < 1) || (n > HOMmax)) {
     cout << "set_bn: n < 1 (" << n << ")" << endl;
     exit(1);
   }
 
-  elem = Cell[Elem_GetPos(Fnum, Knum)].Elem;
+  M = static_cast<MpoleType*>(Cell[Elem_GetPos(Fnum, Knum)].Elem);
 
   if (type.compare("dsgn") == 0) {
-    elem.M->bnpar[HOMmax+n] = bn; elem.M->bnpar[HOMmax-n] = an;
+    M->bnpar[HOMmax+n] = bn; M->bnpar[HOMmax-n] = an;
   } else if (type.compare("sys") == 0) {
-    elem.M->bnsys[HOMmax+n] = bn; elem.M->bnsys[HOMmax-n] = an;
+    M->bnsys[HOMmax+n] = bn; M->bnsys[HOMmax-n] = an;
   } else if (type.compare("rms") == 0) {
-    elem.M->bnrms[HOMmax+n] = bn; elem.M->bnrms[HOMmax-n] = an;
+    M->bnrms[HOMmax+n] = bn; M->bnrms[HOMmax-n] = an;
   } else {
     cout << "get_bn: undef. type" << type << endl;
     exit(1);
