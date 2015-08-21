c1cb33aca8177d05e34e87403d94e2534fa90f4e
Johan Bengtsson <jbngtssn@gmail.com>
2015-08-21 15:02:05 -0400

    Tweaked ele2tracy.py.

--- scsi_src/python/ele2tracy.py
+++ scsi_src/python/ele2tracy.py
@@ -1,32 +1,42 @@
 import math
 import numpy
 import re
+import StringIO
 
 # Module to translate from ELEGANT to Tracy-2,3 lattice.
 
 home_dir = '/home/bengtsson/vladimir/'
 
+# Elegant -> Tracy-2,3 dictionary.
+ele2tracy = { 'watch' : 'marker', 'charge' : 'N/A'}
+
+
 def rd_lines(file_name):
+    str = file_name.split('.')[0]+'.lat'
+    f = open(str, 'w')
     for line in open(home_dir+file_name):
-        line = line.rstrip('\n')
+        line = line.strip('\n').lower()
 #        print '%s' % (line)
         if not line.rstrip():
             # Blank line.
-            print '# blank line'
+            f.write('\n')
             continue
         elif line.startswith('!'):
             # Comment.
-            print '# comment'
+            f.write('{ %s }\n' % (line.strip('!')))
             continue
         elif line.startswith('%'):
             # Declaration.
             token = re.split(r'[;,=\s]\s*', line)
-            print '# definition: %-10s %-10s' % (token[1], token[3])
-            continue
+            f.write('%s = %s;\n' % (token[3], token[1]))
         else:
             token = re.split(r'[;,=\s]\s*', line)
             if token[0].endswith(':'):
-                print 'assignment', token[1]
+                if token[0] == 'c:':
+                    f.write('{ %s }' % (line))
+                else:
+                    f.write('%s %s;\n' % (token[0], ele2tracy[token[1]]))
+                    continue
 
 
 rd_lines('lattice2p0_v1_20150522.lte')
