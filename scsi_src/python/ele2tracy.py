23dd6fa010e31685c5a3b416378d1d395a9a9957
Johan Bengtsson <jbngtssn@gmail.com>
2015-08-21 17:27:47 -0400

    Tweaked ele2tracy.py.

--- scsi_src/python/ele2tracy.py
+++ scsi_src/python/ele2tracy.py
@@ -8,7 +8,10 @@ import StringIO
 home_dir = '/home/bengtsson/vladimir/'
 
 # Elegant -> Tracy-2,3 dictionary.
-ele2tracy = { 'watch' : 'marker', 'charge' : 'N/A'}
+ele2tracy = {
+    'watch' : 'marker', 'charge' : 'N/A', 'rfca' : 'cavity', 'drift' : 'drift',
+    'line' : ''
+    }
 
 
 def rd_lines(file_name):
@@ -16,27 +19,31 @@ def rd_lines(file_name):
     f = open(str, 'w')
     for line in open(home_dir+file_name):
         line = line.strip('\r\n').lower()
-#        print '%s' % (line)
+        print '%s' % (line)
         if not line.rstrip():
             # Blank line.
             f.write('\n')
-            continue
         elif line.startswith('!'):
             # Comment.
             f.write('{ %s }\n' % (line.strip('!')))
-            continue
         elif line.startswith('%'):
             # Declaration.
             token = re.split(r'[;,=\s]\s*', line)
             f.write('%s = %s;\n' % (token[3], token[1]))
         else:
             token = re.split(r'[;,=\s]\s*', line)
+            print token
             if token[0].endswith(':'):
+                # Definition.
+                print 'definition'
                 if token[0] == 'c:':
-                    f.write('{ %s }' % (line))
+                    # Charge definition.
+                    f.write('{ %s }\n' % (line))
+                elif token[1] == 'line':
+                    str = line.split('=')[1]
+                    f.write('%s %s;\n' %  (token[0], str.strip('()')))
                 else:
                     f.write('%s %s;\n' % (token[0], ele2tracy[token[1]]))
-                    continue
 
 
 rd_lines('lattice2p0_v1_20150522.lte')
