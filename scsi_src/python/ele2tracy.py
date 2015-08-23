2aa609134cd73f0b6ffcee98cf15540bae7bcd40
Johan Bengtsson <jbngtssn@gmail.com>
2015-08-23 18:05:37 -0400

    Tweaked ele2tracy.py.

--- scsi_src/python/ele2tracy.py
+++ scsi_src/python/ele2tracy.py
@@ -9,41 +9,100 @@ home_dir = '/home/bengtsson/vladimir/'
 
 # Elegant -> Tracy-2,3 dictionary.
 ele2tracy = {
-    'watch' : 'marker', 'charge' : 'N/A', 'rfca' : 'cavity', 'drift' : 'drift',
-    'line' : ''
+    'charge'    : 'N/A',
+    'mark'      : 'marker',
+    'watch'     : 'marker',
+    'ematrix'   : 'marker',
+    'rfca'      : 'cavity',
+    'drif'      : 'drift',
+    'drift'     : 'drift',
+    'csrdrif'   : 'drift',
+    'csrcsbend' : 'bending',
+    'quad'      : 'quadrupole',
+    'sext'      : 'sextupole',
+    'line'      : ''
     }
+
+
+def parse_rpnc(stack):
+    # Reverse Polish Notation calculator.
+    arg = stack.pop()
+    if arg in '+-*/':
+        op = arg
+        arg = parse_rpnc(stack)
+        return parse_rpnc(stack) + (' %s %s' % (op, arg))
+    else:
+        return ('%s' % (arg))
+
+
+def parse_decl(decl):
+    stack = (decl.strip('%')).split()
+    lhs = stack.pop()
+    op = stack.pop()
+    return ('%s = ' % (lhs)) + parse_rpnc(stack)
+
+
+def parse_line(line, outf):
+    line_lc = line.lower()
+    if not line_lc.rstrip():
+        # Blank line.
+        outf.write('\n')
+    elif line_lc.startswith('!'):
+        # Comment.
+        outf.write('{ %s }\n' % (line.strip('!')))
+    elif line_lc.startswith('%'):
+        # Declaration.
+        outf.write('%s;\n' % (parse_decl(line.strip('%'))))
+    else:
+#        tokens = re.split(r'[,\s]\s*', line_lc)
+        tokens = re.split(r'[,:\s]\s*', line_lc)
+#        print tokens
+        if line_lc <= ':':
+            # Definition.
+            if tokens[1] == 'twiss':
+                # Ignore.
+                a = 1
+        elif tokens[1] == 'charge':
+            # Charge definition.
+            outf.write('{ %s }\n' % (line_lc))
+        elif tokens[1] == 'mark':
+            outf.write('%s %s;\n' % (tokens[0], ele2tracy[tokens[1]]))
+        elif tokens[1] == 'watch':
+            outf.write('%s %s;\n' % (tokens[0], ele2tracy[tokens[1]]))
+        elif tokens[1] == 'rfca':
+            outf.write('%s %s;\n' % (tokens[0], ele2tracy[tokens[1]]))
+            print line_lc
+            print tokens
+            print tokens.index('rfca')
+            exit()
+        elif tokens[1] == 'line':
+            str = line_lc.split('=')[1]
+            outf.write('%s %s;\n' % (tokens[0], str.strip('()')))
+        else:
+            print line_lc
+            print tokens
+            exit()
+            outf.write('not defined\n')
+            outf.write('%s;\n' % (line_lc))
 
 
 def rd_lines(file_name):
     str = file_name.split('.')[0]+'.lat'
-    f = open(str, 'w')
-    for line in open(home_dir+file_name):
-        line = line.strip('\r\n').lower()
-        print '%s' % (line)
-        if not line.rstrip():
-            # Blank line.
-            f.write('\n')
-        elif line.startswith('!'):
-            # Comment.
-            f.write('{ %s }\n' % (line.strip('!')))
-        elif line.startswith('%'):
-            # Declaration.
-            token = re.split(r'[;,=\s]\s*', line)
-            f.write('%s = %s;\n' % (token[3], token[1]))
-        else:
-            token = re.split(r'[;,=\s]\s*', line)
-            print token
-            if token[0].endswith(':'):
-                # Definition.
-                print 'definition'
-                if token[0] == 'c:':
-                    # Charge definition.
-                    f.write('{ %s }\n' % (line))
-                elif token[1] == 'line':
-                    str = line.split('=')[1]
-                    f.write('%s %s;\n' %  (token[0], str.strip('()')))
-                else:
-                    f.write('%s %s;\n' % (token[0], ele2tracy[token[1]]))
+    inf = open(home_dir+file_name, 'r')
+    outf = open(str, 'w')
+    line = inf.readline()
+    while line:
+        line = line.strip('\r\n')
+        while line.endswith('&'):
+            # Line
+            line = line.strip('&')
+            line += (inf.readline()).strip('\r\n')
+            print line
+#        print '%s' % (line)
+
+        parse_line(line, outf)
+
+        line = inf.readline()
 
 
 rd_lines('lattice2p0_v1_20150522.lte')
